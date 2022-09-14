9ddb7c5be22ba79458ee2989e268ab94ba1e3ebe
maxarvid <max.arvid.anderson@gmail.com>
2022-09-14 18:50:56 +0200

    🧪 adds first tests for characters list on initial render

--- cypress/e2e/userVisitsApplication.cy.js
+++ cypress/e2e/userVisitsApplication.cy.js
@@ -1,8 +1,16 @@
 describe("When the user visits the application", () => {
   beforeEach(() => {
+    cy.intercept("GET", "**/people", {
+      fixture: "charactersIndexResponse.json",
+    }).as("charactersIndex");
     cy.visit("/");
   });
-  it("is expected to render hello world", () => {
-    cy.get("body").should("contain.text", "Hello World!");
+
+  it("is expected to make an http request of type GET", () => {
+    cy.wait("@charactersIndex").its("request.method").should("eq", "GET");
   });
-}); 
+
+  it("is expected to render a list of star wars characters", () => {
+    cy.get("[data-cy=star-wars-chars]").should("have.length", 6);
+  });
+});

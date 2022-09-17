ecb63f3d5c2ed029a6ec162cdf4f4c98fdfc1e92
maxarvid <max.arvid.anderson@gmail.com>
2022-09-17 11:26:35 +0200

    🧪 adds network error spec

--- cypress/e2e/userVisitsApplication.cy.js
+++ cypress/e2e/userVisitsApplication.cy.js
@@ -1,34 +1,58 @@
 describe("When the user visits the application", () => {
-  beforeEach(() => {
-    cy.intercept("GET", "**/people", {
-      fixture: "charactersIndexResponse.json",
-    }).as("charactersIndex");
-    cy.visit("/");
-  });
-
-  it("is expected to make an http request of type GET", () => {
-    cy.wait("@charactersIndex").its("request.method").should("eq", "GET");
-  });
-
-  it("is expected to render a list of star wars characters", () => {
-    cy.get("[data-cy=star-wars-chars]").children().should("have.length", 6);
-  });
-
-  it("is expected to show a loading message while fetching data", () => {
-    let resolveReq;
-    cy.intercept("GET", "**/api/people", (req) => {
-      return new Cypress.Promise((resolve) => {
-        resolveReq = resolve;
-      }).then(req.reply);
+  describe("successfully", () => {
+    beforeEach(() => {
+      cy.intercept("GET", "**/people", {
+        fixture: "charactersIndexResponse.json",
+      }).as("charactersIndex");
+      cy.visit("/");
     });
-    cy.visit("/");
-    cy.get("[data-cy=loading-message").should("contain.text", "Loading...");
+
+    it("is expected to make an http request of type GET", () => {
+      cy.wait("@charactersIndex").its("request.method").should("eq", "GET");
+    });
+
+    it("is expected to render a list of star wars characters", () => {
+      cy.get("[data-cy=star-wars-chars]").children().should("have.length", 6);
+    });
+
+    it("is expected to show a loading message while fetching data", () => {
+      let resolveReq;
+      cy.intercept("GET", "**/api/people", (req) => {
+        return new Cypress.Promise((resolve) => {
+          resolveReq = resolve;
+        }).then(req.reply);
+      });
+      cy.visit("/");
+      cy.get("[data-cy=loading-message").should("contain.text", "Loading...");
+    });
+
+    it("is expected to list Luke Skywalker as the first character", () => {
+      cy.get("[data-cy=star-wars-chars")
+        .children()
+        .first()
+        .should("contain.text", "Luke Skywalker");
+    });
   });
 
-  it("is expected to list Luke Skywalker as the first character", () => {
-    cy.get("[data-cy=star-wars-chars")
-      .children()
-      .first()
-      .should("contain.text", "Luke Skywalker");
+  describe.only("unsuccessfully", () => {
+    describe("on network error", () => {
+      beforeEach(() => {
+        cy.intercept("GET", "**/api/people", {
+          forceNetworkError: true,
+        }).as("networkError");
+        cy.visit("/");
+      });
+
+      it("is expected to respond with a network error", () => {
+        cy.wait("@networkError").should("have.property", "error");
+      });
+
+      it("is expected to inform user something went wrong", () => {
+        cy.get("[data-cy=error-message]").should(
+          "contain.text",
+          "Network error, please try again later..."
+        );
+      });
+    });
   });
 });
